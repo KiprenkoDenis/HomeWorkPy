32620deaf1bf99230679311219472f05afed9216
Denis_Kiprenko <deniskiprenko@yandex.ru>
2023-07-08 17:53:41 +1000

    добавил Д.З.5

---
+++ H_W_5/H_W_5.1.py
@@ -0,0 +1,18 @@
+# Напишите программу, которая на вход принимает
+# два числа A и B, и возводит число А в целую степень B с
+# помощью рекурсии.
+
+a = int(input('Укажите число A: '))
+b = int(input('Укажите число B: '))
+
+def exponentiation(A, B):
+    if B == 0:
+        return 1
+    elif B < 0:
+        return 1 / exponentiation(A, -B)
+    else:
+        return A * exponentiation(A, B - 1)
+
+
+result = exponentiation(a, b)
+print(f"A = {a}; B = {b} -> {result}")
